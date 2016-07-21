344ad10f4212c5d4342a49a972e68d2814ccc54b
laushinka <laurie.malau@gmail.com>
2016-07-21 18:32:56 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $("p").click(function(){
+    alert("Hey!");
+  });
+};
+
+function frameIt() {
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $("#typing").keydown(function(e){
+    if (e.which === 71) {
+      alert("");
+    }
+  })
+}
+
+function submitIt() {
+  $("form").submit(function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
